830b1f5d5bd86f12483ac47b2c12450438fbba8f
sakthi16666 <88278639+sakthi16666@users.noreply.github.com>
2022-07-08 19:09:03 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+// function cl() {
+//     var data = document.getElementById("BYclick");
+//     if (data.style.display !== "none") {
+//         data.style.display = "none";
+//     }
+//     else {
+//         data.style.display = "block";
+//     }
+// }
